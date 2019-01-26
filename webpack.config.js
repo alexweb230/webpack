503ca21e8b374548290d360df07c4cfee561f866
Александр <alex230pust@gmail.com>
2019-01-26 20:52:28 +0200

    web

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,23 @@ module.exports = {
                     'img-loader',
                 ]
             },
+            {
+                test:/\.{woff|woff2|eot|ttf|otf}$/,
+                use:[
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            name: '[path}[name].[ext]',
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-url-loader',
+
+            },
+
         ],
     },
     plugins: [
